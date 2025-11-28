8efe58e075302327033f8f318cdcca48079ff238
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2025-11-29 03:12:59 +0600

    185

--- B_Addition_on_a_Segment.cpp
+++ B_Addition_on_a_Segment.cpp
@@ -10,31 +10,18 @@ void sol()
 {
     int n;
     cin >> n;
-
-    vector<int>v(n);
+    int sum = 0;
+    int cnt =0;
     for (int i = 0; i < n; i++)
     {
-        cin>>v[i];
+        int x;
+        cin>>x;
+
+        sum+=x;
+        if(x) cnt++;
     }
 
-    sort(v.begin(),v.end());
-
-    int ans=0;
-    int a = *max_element(v.begin(), v.end());
-    int x;
-    vector<int>s;
-    for (int i = 0; i < n; i++)
-    {
-        if(0!=v[i]){
-            s.push_back(v[i]);
-        }
-    }
-    int sz = s.size();
-    if(s[0]==s[sz-1]){
-        cout<<1<<nl;
-    }else{
-        cout<<sz<<endl;
-    }
+    cout<<min(sum-n+1,cnt)<<endl;
 }
 signed main()
 {
