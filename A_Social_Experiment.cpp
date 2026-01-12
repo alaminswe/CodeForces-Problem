5d037424685881cf598379e69219906e587a0052
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-01-13 04:42:42 +0600

    Codeforce

---
+++ A_Social_Experiment.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define bismillah()               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define int long long
+#define nl '\n'
+void sol()
+{
+    int n;
+    cin >> n;
+
+    if (n <= 3)
+        cout << n << '\n';
+    else
+        cout << (n % 2) << '\n';
+}
+signed main()
+{
+    bismillah();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        sol();
+    }
+    return 0;
+}
