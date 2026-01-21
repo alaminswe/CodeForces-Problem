9df0785cf12e9cd36dd8d7869cd65c1cc977b4d1
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-01-21 07:26:24 +0600

    Codeforce

--- B_MEX_Reordering.cpp
+++ B_MEX_Reordering.cpp
@@ -3,30 +3,24 @@ using namespace std;
 #define bismillah() ios_base::sync_with_stdio(0); cin.tie(0);
 #define int long long
 
-int mex(int l, int r, vector<int> &v) {
-    unordered_set<int> s;
-    for(int i = l; i <= r; i++){
-        s.insert(v[i]);
-    }
-    int m = 0;
-    while(s.count(m)) m++;
-    return m;
-}
-
-
 void solve()
 {
     int n;cin>>n;
     map<int,int> mp;
-    for(int a,i=0; i<n; i++) 
+    for(int x,i=0; i<n; i++) 
     {
-        cin>>a;
-        mp[a]++;
+        cin>>x;
+        mp[x]++;
     }
-    if(mp[0] == 0) {cout<<"YES"<<endl; return;}
-    if(mp[0] == 1) {cout<<"YES"<<endl; return;}
-    if(mp[1] == 0) {cout<<"NO"<<endl; return;}
-    cout<<"YES"<<endl;
+    if(mp[0] == 1) {
+        cout<<"YES"<<endl; 
+        return;
+    }
+    if(mp[0] > 1 && mp[1] >= 1) {
+        cout<<"YES"<<endl; 
+        return;
+    }
+    cout<<"NO"<<endl;
 }
  
 signed main()
