8fe13100aa8d6b10e7091348dbaaf0604cf225b8
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-04-07 00:19:39 +0600

    CodeForce

--- D_The_67_th_OEIS_Problem.cpp
+++ D_The_67_th_OEIS_Problem.cpp
@@ -3,17 +3,25 @@ using namespace std;
 #define bismillah() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
 #define nl '\n'
-
+vector<int>primes;
 void sol()
 {
     int n;
     cin >> n;
-
-    cout << 1 << " ";
-    for (int i = 2; i <= n; i++)
+    vector<int> result;
+    for (int i = 0; i < n; i++)
     {
-        cout << i * (i - 1) << " ";
+        if(i == 0) result.push_back(primes[i]);
+        else{
+            int x = primes[i]*primes[i-1];
+            result.push_back(x);
+        }
     }
+    for (auto u : result)
+    {
+        cout<<u<< " ";
+    }
+    
     cout << nl;
 }
 
@@ -22,6 +30,22 @@ signed main()
     bismillah();
     int t;
     cin >> t;
+    for (int i = 1; i <= 1000000; i++) {
+        bool is_prime = false;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                is_prime = true; 
+                break;
+            }
+        }
+        if (!is_prime) primes.push_back(i);
+    }
+
+    // for (auto u : primes)
+    // {
+    //     cout<<u<< " ";
+    // }
+    
     while (t--)
     {
         sol();
