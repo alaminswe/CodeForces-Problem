8ac9cfba83ad0f792e264fc09a88570221c865b9
Md. Alamin Ridoy <134636892+Alaminridoy@users.noreply.github.com>
2026-04-15 22:07:03 +0600

    CODECHEF

--- b.cpp
+++ b.cpp
@@ -6,43 +6,29 @@ using namespace std;
 
 void sol()
 {
-    string s;
-    cin>>s;
-    int n = s.size();
-
-    vector<int>v;
-    int cnt = 0;
-
-    for(char c:s) {
-        if(c == '1') {
-            cnt++;
-        } else {
-            if(cnt > 0) {
-                v.push_back(cnt);
-                cnt = 0;
-            }
+        int n;
+        cin >> n;
+        
+        string s = "";
+        
+        int zero = n / 2;
+        int one = n - zero;
+        
+        for (int i = 0; i < zero; i++) {
+            s += '0';
         }
-    }
-
-    if(cnt > 0) v.push_back(cnt);
-
-    
-    sort(v.rbegin(), v.rend());
-
-    int z = 0;
-    int a = v.size();
-    for(int i = 0; i < a ; i += 2) {
-        z += v[i];
-    }
-
-    cout << z <<nl;
-
+        
+        for (int i = 0; i < one; i++) {
+            s += '1';
+        }
+        
+        cout << s << nl;
 }
 
 signed main()
 {
     bismillah();
-    int t=1;
+    int t = 1;
     cin >> t;
     while (t--)
     {
